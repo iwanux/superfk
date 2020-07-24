2f981e14104dc86dff6d281181abcb2cb997d1f8
Iwan Darmawan <Iwan Darmawan>
2020-07-24 11:29:12 +0700

    Updates

---
+++ precache-manifest.e4a327bb7134ae5e099e6511a2f89e0b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d88ca097a0aadc388f1ab9c59943886",
+    "url": "/superfk/index.html"
+  },
+  {
+    "revision": "25b2645a27dac9e385a7",
+    "url": "/superfk/static/css/main.c374a8b0.chunk.css"
+  },
+  {
+    "revision": "2226324ca2cbe43ee1e7",
+    "url": "/superfk/static/js/2.0ebc03d3.chunk.js"
+  },
+  {
+    "revision": "b7bdffd4fd4ff576b1a1d037e48d8a27",
+    "url": "/superfk/static/js/2.0ebc03d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25b2645a27dac9e385a7",
+    "url": "/superfk/static/js/main.61066f9e.chunk.js"
+  },
+  {
+    "revision": "1b4fb47d177395515a3b",
+    "url": "/superfk/static/js/runtime-main.b06c94eb.js"
+  },
+  {
+    "revision": "7fb3e07f1c659d01da0a1a5c6132eb97",
+    "url": "/superfk/static/media/GettyImages-694542042-e1586274805503.7fb3e07f.jpg"
+  },
+  {
+    "revision": "161fa091acc23e0c73730c199c83fb1c",
+    "url": "/superfk/static/media/f68217cb8d16716e729b3fe2c8d2b425.161fa091.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/superfk/static/media/logo.5d5d9eef.svg"
+  }
+]);
